ee68a0848858f1d790c6acad5e72c95b1bb71de4
Steve Bennett <66376+stebennett@users.noreply.github.com>
2021-03-11 10:19:08 +0000

    Adds datadog event collector boilerplate

---
+++ collectors/datadog-event-collector/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    application
+    kotlin("jvm")
+    kotlin("kapt")
+}
+
+dependencies {
+    // Kotlin
+    implementation(platform(Dependencies.kotlinBom))
+    implementation(Dependencies.kotlinStdLibJdk8)
+    implementation(Dependencies.guava)
+
+    // HttpClient
+    Dependencies.httpClientCore.forEach { implementation(it) }
+    kapt(Dependencies.moshiCodeGen)
+
+    // Databases
+    Dependencies.dbCore.forEach { implementation(it) }
+
+    // Tests
+    Dependencies.testsCore.forEach { testImplementation(it) }
+}
+
+application {
+    mainClass.set("squaddashboard.AppKt")
+}
---
+++ collectors/datadog-event-collector/src/main/kotlin/squaddashboard/collectors/datadogevents/App.kt
@@ -0,0 +1,5 @@
+package squaddashboard.collectors.datadogevents
+
+fun main() {
+    println("Hello, World!")
+}
