4891ca4c4b36af34e3787994aa6897f812b2907a
Steve Bennett <66376+stebennett@users.noreply.github.com>
2021-12-27 17:03:53 +0000

    Initial implementation of making a Jira Request

--- cmd/jiracollector/main.go
+++ cmd/jiracollector/main.go
@@ -1,15 +1,76 @@
 package main
 
 import (
-	"flag"
-
-	"github.com/golang/glog"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-var version = "?"
+type JiraSearchQuery struct {
+	jql    string
+	fields []string
+	expand []string
+}
 
 func main() {
-	flag.Parse()
-	glog.CopyStandardLogTo("INFO")
-	glog.Infof("Version: %v", version)
+	jiraBaseUrl := os.Getenv("JIRA_HOST")
+	if jiraBaseUrl == "" {
+		log.Fatal("JIRA_HOST env var is not set")
+	}
+
+	jiraUser := os.Getenv("JIRA_USER")
+	if jiraUser == "" {
+		log.Fatal("JIRA_USER env var is not set")
+	}
+
+	jiraAuthToken := os.Getenv("JIRA_AUTH_TOKEN")
+	if jiraAuthToken == "" {
+		log.Fatal("JIRA_AUTH_TOKEN env var is not set")
+	}
+
+	jiraQuery := os.Getenv("JIRA_QUERY")
+	if jiraQuery == "" {
+		log.Fatal("JIRA_QUERY env var is not set")
+	}
+
+	jiraEpicField := os.Getenv("JIRA_EPIC_FIELD")
+	if jiraEpicField == "" {
+		log.Fatal("JIRA_EPIC_FIELD env var is not set")
+	}
+
+	url := fmt.Sprintf("https://%s/jira/rest/api/2/search", jiraBaseUrl)
+
+	query := &JiraSearchQuery{
+		jql:    jiraQuery,
+		fields: []string{"summary", "issuetype", jiraEpicField},
+		expand: []string{"changelog"},
+	}
+
+	queryJSON, err := json.Marshal(query)
+	if err != nil {
+		log.Fatal("Failed to create Jira Query in JSON")
+	}
+
+	jiraClient := http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(queryJSON))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.SetBasicAuth(jiraUser, jiraAuthToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	response, err = jiraClient.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to make request. Got error %s", err.Error())
+	}
+
+	defer response.Body.Close()
 }
