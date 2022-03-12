944690a118500f3cc88e81caccaf1d5e673fb87b
Steve Bennett <66376+stebennett@users.noreply.github.com>
2022-03-12 18:15:45 +0000

    A little more refactoring into services

---
+++ cmd/jiracollector/jiracollector.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/stebennett/squad-dashboard/cmd/jiracollector/models"
+	"github.com/stebennett/squad-dashboard/cmd/jiracollector/repository"
+	"github.com/stebennett/squad-dashboard/pkg/jiraservice"
+	"github.com/stebennett/squad-dashboard/pkg/util"
+)
+
+type JiraCollector struct {
+	repo repository.IssueRepository
+	jira *jiraservice.JiraService
+}
+
+func NewJiraCollector(repo repository.IssueRepository, jira *jiraservice.JiraService) *JiraCollector {
+	return &JiraCollector{
+		repo: repo,
+		jira: jira,
+	}
+}
+
+func (jc *JiraCollector) execute(jql string, epicField string) {
+
+	query := jiraservice.JiraSearchQuery{
+		Jql:        jql,
+		Fields:     []string{"summary", "issuetype", epicField},
+		Expand:     []string{"changelog"},
+		StartAt:    0,
+		MaxResults: 100,
+	}
+
+	log.Printf("Querying Jira for startAt: %d; maxResults: %d", query.StartAt, query.MaxResults)
+	searchResult, err := jc.jira.MakeJiraSearchRequest(&query)
+	if err != nil {
+		log.Fatalf("Failed to make request %s", err)
+	}
+
+	for _, issue := range searchResult.Issues {
+		saveableIssue := models.Create(issue)
+		go jc.repo.StoreIssue(context.Background(), saveableIssue)
+	}
+
+	var nextPageStartAt = util.NextPaginationArgs(0, 100, len(searchResult.Issues), searchResult.Total)
+	for {
+		if nextPageStartAt == -1 {
+			log.Println("No new pages to fetch.")
+			break
+		}
+
+		query.StartAt = nextPageStartAt
+
+		log.Printf("Querying Jira for startAt: %d; maxResults: %d; total: %d", query.StartAt, query.MaxResults, searchResult.Total)
+		searchResult, err := jc.jira.MakeJiraSearchRequest(&query)
+		if err != nil {
+			log.Fatalf("Failed to make request %s; startAt: %d", err, nextPageStartAt)
+		}
+
+		nextPageStartAt = util.NextPaginationArgs(nextPageStartAt, 100, len(searchResult.Issues), searchResult.Total)
+	}
+}
