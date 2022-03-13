7bfbf8498f817aad96f6a7c9b29fc5a5ba82d0f8
Steve Bennett <66376+stebennett@users.noreply.github.com>
2022-03-13 17:20:58 +0000

    Set to recursive function

--- cmd/jiracollector/jiracollector/jiracollector.go
+++ cmd/jiracollector/jiracollector/jiracollector.go
@@ -23,43 +23,7 @@ func NewJiraCollector(repo repository.IssueRepository, jira *jiraservice.JiraSer
 }
 
 func (jc *JiraCollector) Execute(jql string, epicField string) {
-
-	query := jiraservice.JiraSearchQuery{
-		Jql:        jql,
-		Fields:     []string{"summary", "issuetype", epicField},
-		Expand:     []string{"changelog"},
-		StartAt:    0,
-		MaxResults: 100,
-	}
-
-	log.Printf("Querying Jira for startAt: %d; maxResults: %d", query.StartAt, query.MaxResults)
-	searchResult, err := jc.jira.MakeJiraSearchRequest(&query)
-	if err != nil {
-		log.Fatalf("Failed to make request %s", err)
-	}
-
-	for _, issue := range searchResult.Issues {
-		saveableIssue := models.Create(issue)
-		go jc.repo.StoreIssue(context.Background(), saveableIssue)
-	}
-
-	var nextPageStartAt = util.NextPaginationArgs(0, 100, len(searchResult.Issues), searchResult.Total)
-	for {
-		if nextPageStartAt == -1 {
-			log.Println("No new pages to fetch.")
-			break
-		}
-
-		query.StartAt = nextPageStartAt
-
-		log.Printf("Querying Jira for startAt: %d; maxResults: %d; total: %d", query.StartAt, query.MaxResults, searchResult.Total)
-		searchResult, err := jc.jira.MakeJiraSearchRequest(&query)
-		if err != nil {
-			log.Fatalf("Failed to make request %s; startAt: %d", err, nextPageStartAt)
-		}
-
-		nextPageStartAt = util.NextPaginationArgs(nextPageStartAt, 100, len(searchResult.Issues), searchResult.Total)
-	}
+	jc.execute(0, 100, jql, epicField)
 }
 
 func (jc *JiraCollector) execute(startAt int, maxResults int, jql string, epicField string) {
