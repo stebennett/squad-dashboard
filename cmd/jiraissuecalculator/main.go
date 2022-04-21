01adcd2d4821b22bf8b0e85ccc27b3aa6aa068d0
Steve Bennett <66376+stebennett@users.noreply.github.com>
2022-04-21 10:27:08 +0100

    todos for calculator

--- cmd/jiraissuecalculator/main.go
+++ cmd/jiraissuecalculator/main.go
@@ -3,5 +3,15 @@ package main
 import "log"
 
 func main() {
+	// fetch all issues and set create year-week
+
+	// fetch all issues started and set started year-week
+
+	// fetch all issues completed and set cycle time
+
+	// fetch all issues completed and set lead time
+
+	// fetch all issues completed and set complete year-week
+
 	log.Fatal("Not yet implemented")
 }
