45311b79ec7b144c388151997b40c5609f736d94
Steve Bennett <66376+stebennett@users.noreply.github.com>
2022-07-04 13:37:40 +0100

    adds code to store pd on-calls

--- cmd/pagerdutyoncallcollector/main.go
+++ cmd/pagerdutyoncallcollector/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/Netflix/go-env"
+	"github.com/stebennett/squad-dashboard/pkg/pagerdutyrepository"
 	"github.com/stebennett/squad-dashboard/pkg/pagerdutyservice"
 )
 
@@ -23,6 +28,9 @@ func main() {
 	// create a pagerduty service
 	pagerduty := createPagerDutyService(environment)
 
+	// create a pagerduty service
+	pagerdutyrepository := createPagerDutyRepository()
+
 	since, _ := time.Parse(time.RFC3339, "2022-06-01T00:00:00Z")
 	until, _ := time.Parse(time.RFC3339, "2022-06-30T23:59:59Z")
 
@@ -34,6 +42,11 @@ func main() {
 	log.Printf("found %d on-calls", len(oncalls))
 
 	for _, oncall := range oncalls {
+		_, err = pagerdutyrepository.SaveOnCall(context.Background(), oncall)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		log.Printf("on-call found - start[%s], end[%s], person[%s], escalation[%s], schedule[%s], level[%d]",
 			oncall.Start.Format(time.RFC3339),
 			oncall.End.Format(time.RFC3339),
@@ -56,4 +69,18 @@ func createPagerDutyService(environment Environment) *pagerdutyservice.PagerDuty
 	}
 
 	return pagerdutyservice.NewPagerDutyService(&client, params)
+}
+
+func createPagerDutyRepository() pagerdutyrepository.PagerDutyRepository {
+	var err error
+	var db *sql.DB
+	connStr := os.ExpandEnv("postgres://$DB_USERNAME:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable") // load from env vars
+
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database initialised")
+	return pagerdutyrepository.NewPostgresPagerDutyRepository(db)
 }
