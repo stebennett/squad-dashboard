c6d7ee8ac57a3ed5c29e22877b3710e18519bff0
Steve Bennett <66376+stebennett@users.noreply.github.com>
2022-07-28 12:22:29 +0100

    add basic api health check

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "log"
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	mux "github.com/gorilla/mux"
+)
 
 func main() {
-	log.Fatalln("API Started")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/v1/health", HealthHandler).Methods("GET")
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         os.ExpandEnv(":$PORT"),
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Printf("Server Started")
+
+	<-done
+	log.Print("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// add extra close handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed: %+v", err)
+	}
+
+	log.Print("Server Shutdown")
+}
+
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 }
