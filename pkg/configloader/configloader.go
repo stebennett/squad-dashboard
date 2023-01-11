093d320b71250e937ccbbfb42f646affb96acc1f
Steve Bennett <66376+stebennett@users.noreply.github.com>
2023-01-11 16:11:11 +0000

    improve error passing

--- pkg/configloader/configloader.go
+++ pkg/configloader/configloader.go
@@ -19,22 +19,22 @@ type Config struct {
 func Load(ctx context.Context, repo configrepository.ConfigRepository, config Config) error {
 	_, err := repo.SaveJiraToDoStates(context.Background(), config.JiraProject, config.JiraToDoStates)
 	if err != nil {
-		return errors.New(fmt.Sprintf("failed to save Jira Work ToDo States. %s", err))
+		return fmt.Errorf("failed to save Jira Work ToDo States. %s", err)
 	}
 
 	_, err = repo.SaveJiraInProgressStates(context.Background(), config.JiraProject, config.JiraInProgressStates)
 	if err != nil {
-		return errors.New(fmt.Sprintf("failed to save Jira Work In Progress States. %s", err))
+		return fmt.Errorf("failed to save Jira Work In Progress States. %s", err)
 	}
 
 	_, err = repo.SaveJiraDoneStates(context.Background(), config.JiraProject, config.JiraDoneStates)
 	if err != nil {
-		return errors.New(fmt.Sprintf("Failed to save Jira Work Done States. %s", err))
+		return fmt.Errorf("failed to save Jira Work Done States. %s", err)
 	}
 
 	_, err = repo.SaveNonWorkingDays(context.Background(), config.JiraProject, config.NonWorkingDays)
 	if err != nil {
-		return errors.New(fmt.Sprintf("Failed to save non working days. %s", err))
+		return fmt.Errorf("failed to save non working days. %s", err)
 	}
 
 	return nil
