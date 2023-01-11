c4c8143d0d3cb13f8b94d6eb81c4fb52cec9c12f
Steve Bennett <66376+stebennett@users.noreply.github.com>
2023-01-11 14:59:11 +0000

    Update report with charts

--- pkg/printer/pdfreportprinter.go
+++ pkg/printer/pdfreportprinter.go
@@ -1,8 +1,13 @@
 package printer
 
 import (
+	"fmt"
 	"image/color"
+	"log"
+	"time"
 
+	"github.com/stebennett/squad-dashboard/pkg/dashboard/models"
+	"github.com/stebennett/squad-dashboard/pkg/jiracalculationsrepository"
 	"github.com/stebennett/squad-dashboard/pkg/report"
 )
 
@@ -24,16 +29,21 @@ func NewPdfReportPrinter(cycleTimeChart string, throughputChart string, escapedD
 
 func (p *PdfReportPrinter) Print(reports Reports) error {
 	reportDashboards := make(map[string]report.ReportDashboard)
+
 	reportDashboards[p.JiraProject] = report.ReportDashboard{
 		Quality: report.ReportDashboardItem{
 			BackgroundColor: color.Black,
+			Chart:           p.EscapedDefectsChart,
 		},
 		Quantity: report.ReportDashboardItem{
 			BackgroundColor: color.Black,
+			Chart:           p.ThroughputChart,
 		},
 		Speed: report.ReportDashboardItem{
 			BackgroundColor: color.Black,
+			Chart:           p.CycleTimeChart,
 		},
+		SpeedAnomalies: getSpeedAnomalies(reports.CycleTimeReports, reports.AllCycleTimes),
 	}
 
 	reportData := report.ReportData{
@@ -42,4 +52,46 @@ func (p *PdfReportPrinter) Print(reports Reports) error {
 
 	err := report.GeneratePdfReport(reportData, "/output/report.pdf")
 	return err
+}
+
+func getSpeedAnomalies(cycleTimesReports []models.WeekCount, allCycleTimes []jiracalculationsrepository.CycleTimes) []report.SpeedAnomaly {
+	speedAnomalies := []report.SpeedAnomaly{}
+
+	for _, ctavg := range cycleTimesReports {
+		cycleTimesInWeek := filterCycleTimes(allCycleTimes, ctavg.WeekEnding.AddDate(0, 0, -7), ctavg.WeekEnding)
+		for _, ct := range cycleTimesInWeek {
+			if ct.Size > ctavg.Count {
+				speedAnomalies = append(speedAnomalies, report.SpeedAnomaly{
+					IssueKey:      ct.IssueKey,
+					Size:          ct.Size,
+					CompletedDate: ct.Completed,
+					Link:          fmt.Sprintf("https://jira/%s", ct.IssueKey),
+				})
+			}
+		}
+	}
+
+	return speedAnomalies
+}
+
+func filterCycleTimes(cycleTimes []jiracalculationsrepository.CycleTimes, startDate time.Time, endDate time.Time) []jiracalculationsrepository.CycleTimes {
+	filteredCycleTimes := []jiracalculationsrepository.CycleTimes{}
+
+	compareBeforeYear, compareBeforeMonth, compareBeforeDate := endDate.Date()
+	compareAfterYear, compareAfterMonth, compareAfterDate := startDate.Date()
+
+	log.Printf("filtering issues between [%d, %d, %d] and [%d, %d, %d]", compareAfterYear, compareAfterMonth, compareAfterDate, compareBeforeYear, compareBeforeMonth, compareBeforeDate)
+
+	for _, ct := range cycleTimes {
+		completedYear, completedMonth, completedDate := ct.Completed.Date()
+
+		if completedYear > compareAfterYear && completedYear <= compareBeforeYear &&
+			completedMonth > compareAfterMonth && completedMonth <= compareBeforeMonth &&
+			completedDate > compareAfterDate && completedDate <= compareBeforeDate {
+			log.Println("Adding item")
+			filteredCycleTimes = append(filteredCycleTimes, ct)
+		}
+	}
+
+	return filteredCycleTimes
 }
--- pkg/report/report.go
+++ pkg/report/report.go
@@ -1,7 +1,10 @@
 package report
 
 import (
+	"fmt"
 	"image/color"
+	"sort"
+	"time"
 
 	"github.com/jung-kurt/gofpdf"
 )
@@ -16,6 +19,14 @@ const (
 
 type ReportDashboardItem struct {
 	BackgroundColor color.Color
+	Chart           string
+}
+
+type SpeedAnomaly struct {
+	IssueKey      string
+	Size          int
+	CompletedDate time.Time
+	Link          string
 }
 
 type ReportDashboard struct {
@@ -23,18 +34,92 @@ type ReportDashboard struct {
 	Quantity ReportDashboardItem
 	Speed    ReportDashboardItem
 
-	SpeedAnomalies []string
+	SpeedAnomalies []SpeedAnomaly
 }
 
 type ReportData struct {
 	Dashboards map[string]ReportDashboard
 }
 
+const (
+	font                 = "Arial"
+	margin               = 10.0
+	headingFontHeight    = 16.0
+	subheadingFontHeight = 14.0
+	fontHeight           = 10.0
+	cellMargin           = 2.0
+	pageWidth            = 210
+)
+
 func GeneratePdfReport(reportData ReportData, outputFile string) error {
 	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetMargins(margin, margin, margin)
 	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Squad Dashboard Report")
+	pdf.SetFont(font, "B", headingFontHeight)
+	pdf.SetCellMargin(cellMargin)
+	pdf.Cell(0, pdf.PointToUnitConvert(headingFontHeight)+2*cellMargin, "Squad Dashboard Report")
+
+	pdf.SetY(pdf.PointToUnitConvert(headingFontHeight) + margin)
+	pdf.SetFont(font, "", fontHeight)
+
+	pdf.SetFont(font, "B", subheadingFontHeight)
+	for k, v := range reportData.Dashboards {
+		addChartPage(pdf, k+" - Quality", v.Quality.Chart)
+		addChartPage(pdf, k+" - Quantity", v.Quantity.Chart)
+		addChartPage(pdf, k+" - Speed", v.Speed.Chart)
+
+		addSpeedAnomaliesTable(pdf, v.SpeedAnomalies)
+	}
 
 	return pdf.OutputFileAndClose(outputFile)
+}
+
+func addChartPage(pdf gofpdf.Pdf, chartTitle string, chartLocation string) *gofpdf.Pdf {
+	opt := gofpdf.ImageOptions{
+		ImageType: "png",
+	}
+
+	pdf.AddPage()
+	pdf.Cell(0, pdf.PointToUnitConvert(subheadingFontHeight), chartTitle)
+	pdf.SetY(pdf.PointToUnitConvert(subheadingFontHeight) + 2*margin)
+	pdf.ImageOptions(chartLocation, margin, margin, pageWidth-(2*margin), 0, true, opt, 0, "")
+	return &pdf
+}
+
+func addSpeedAnomaliesTable(pdf gofpdf.Pdf, anomalies []SpeedAnomaly) *gofpdf.Pdf {
+	const (
+		colCount = 4
+	)
+	pdf.Cell(0, pdf.PointToUnitConvert(subheadingFontHeight), "Anomalies")
+
+	pdf.SetY(pdf.GetY() + margin)
+
+	cellWidth := (pageWidth - (2 * margin) - (colCount * 2 * cellMargin)) / colCount
+	cellHeight := pdf.PointToUnitConvert(fontHeight) + 2*cellMargin
+
+	pdf.SetFont(font, "B", fontHeight)
+
+	// headings
+	pdf.CellFormat(cellWidth, cellHeight, "Date Completed", gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+	pdf.CellFormat(cellWidth, cellHeight, "Issue Key", gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+	pdf.CellFormat(cellWidth, cellHeight, "Cycle Time", gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+	pdf.CellFormat(cellWidth, cellHeight, "Link", gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+
+	pdf.SetFont(font, "B", fontHeight)
+	pdf.SetY(pdf.GetY() + cellHeight)
+
+	sort.Slice(anomalies, func(i, j int) bool {
+		return anomalies[i].CompletedDate.After(anomalies[j].CompletedDate)
+	})
+
+	for _, anomaly := range anomalies {
+		pdf.CellFormat(cellWidth, cellHeight, anomaly.CompletedDate.Format("2006-01-02"), gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+		pdf.CellFormat(cellWidth, cellHeight, anomaly.IssueKey, gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+		pdf.CellFormat(cellWidth, cellHeight, fmt.Sprintf("%d", anomaly.Size), gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, "")
+		pdf.CellFormat(cellWidth, cellHeight, anomaly.Link, gofpdf.BorderFull, 0, gofpdf.AlignLeft, false, 0, anomaly.Link)
+
+		pdf.SetY(pdf.GetY() + cellHeight)
+	}
+
+	return &pdf
 }
